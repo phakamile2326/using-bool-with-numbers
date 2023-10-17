4ae578da9d8cb1547bc6a088a3e05ec411e881d4
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-17 05:01:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace using_bool_with_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(30, 0));
+            Console.WriteLine(test(25,5));
+            Console.WriteLine(test(20,30));
+            Console.WriteLine(test(20,25));
+            Console.ReadLine();
+        }
+        public static bool test (int x, int y)
+        {
+            return x == 30 || y == 30 || (x+y) == 30;
+        }
+        
+    }
+}
+    
+
